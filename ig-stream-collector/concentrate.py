a3c675e2bb9dfbbbce12ef184c97cfb7998793c4
zansibal <abrahamsson.richard@gmail.com>
2022-03-09 16:09:12 +0100

    now working

--- ig-stream-collector/concentrate.py
+++ ig-stream-collector/concentrate.py
@@ -10,12 +10,23 @@ def read_file(file_):
         logging.warning(f'Exception while reading {file_}: {e}')
         return pd.DataFrame()
 
+def get_filename(directory, timestamp):
+    return f'{os.path.basename(directory)}_{timestamp.strftime("%Y-%U")}.ftr'
+
 if __name__ == '__main__':
+    logging.basicConfig(
+        level = logging.INFO,
+        format = '%(asctime)s %(levelname)s %(message)s',
+        handlers = [
+            logging.StreamHandler(), # Prints to stdout
+        ],
+    )
+
     logging.warning('This script should only be run during non-trading hours '
                     '(weekend), so that full weeks can be concentrated')
 
-    path_source = os.path.join(os.path.expanduser('~'), 'data', 'tick')
-    path_dest = os.path.join(os.path.expanduser('~'), 'data', 'tick_weekly')
+    path_source = os.path.join(os.path.expanduser('~'), 'data', 'tick', '*')
+    path_weeks = os.path.join(os.path.expanduser('~'), 'data', 'tick_weekly')
     dirs = glob.glob(path_source)
 
     for directory in dirs:
@@ -27,5 +38,14 @@ if __name__ == '__main__':
             dfs.append(read_file(file_))
             
         df = pd.concat(dfs)
-        os.makedirs(os.path.join(path_dest, directory))
-        df.to_feather(os.path.join(path_dest, directory, f'{filename}.ftr'))
+        df = df.sort_values(by='index')
+
+        path_dest = os.path.join(path_weeks, os.path.basename(directory))
+        if not os.path.exists(path_dest):
+            os.makedirs(path_dest)
+        
+        weeks = [g for n, g in df.groupby(pd.Grouper(key='index',freq='W-SUN'))]
+        for week in weeks:
+            week.reset_index(drop=True).to_feather(
+                os.path.join(path_dest, get_filename(directory, week.iloc[-1,0]))
+            )
